3ba3f4d5137ffb32c5bbaec379ec2a75826bd0f6
Ruslan Samoylenko <ruslan.sem@gmail.com>
2023-02-05 20:58:30 +0200

    Without hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,100 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    const name = event.target.elements.name.value;
+    const number = event.target.elements.number.value;
+    if (this.state.contacts.find(item => item.name === name)) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [
+        ...prevState.contacts,
+        {
+          id: nanoid(),
+          name,
+          number,
+        },
+      ],
+    }));
+  };
+
+  filteredContacts = () =>
+    this.state.contacts.filter(item =>
+      item.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+
+  deleteContact = event => {
+    this.setState({
+      contacts: this.state.contacts.filter(item => item.id !== event.target.id),
+    });
+  };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      const parsedContacts = JSON.parse(savedContacts);
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 20,
+          color: '#010101',
+        }}
+      >
+        <h1
+          style={{
+            margin: 0,
+            fontSize: 32,
+          }}
+        >
+          Phonebook
+        </h1>
+        <ContactForm onSubmit={this.handleSubmit} />
+        <h2
+          style={{
+            margin: 0,
+            fontSize: 24,
+          }}
+        >
+          Contacts
+        </h2>
+        <Filter onChange={this.handleChange} />
+        <ContactList
+          contacts={this.filteredContacts}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
